5bfa18291aaede8f514882a4ae0a19e982ff3e21
Gokul <gokulchandan18@gmail.com>
2022-01-31 19:53:06 +0530

    commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var http =require('http');
 
 var server=http.createServer(function (req,res){
@@ -7,4 +8,17 @@ var server=http.createServer(function (req,res){
 server.listen(3000,'localhost',function(){
     console.log('Server started on port:3000');
 
+});
+
+
+var fs=require('fs');
+
+fs.writeFile('./hello.txt','How are you?',function(err){
+    if(!err){
+        fs.readFile('./hello.txt',function(err,data){
+            if(!err){
+                console.log(data.toString());
+            }
+        });
+    }
 });
