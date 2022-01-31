246c27d2ce051787d47f04a4b18c6ff95202cb7c
Gokul <gokulchandan18@gmail.com>
2022-01-31 20:11:46 +0530

    commit

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ var eventEmitter=new events.EventEmitter();
 
 
 var server=http.createServer(function (req,res){
-    eventEmitter.emit('someone requested');
+    eventEmitter.emit('someone requested','test');
 
     res.end('server works!');
 });
 
 
-eventEmitter.on('someone requested', function(){
-    console.log('a request has been done on the server');
+eventEmitter.on('someone requested', function(data){
+    console.log('a request has been done on the server',data);
 });
 
 server.listen(3000,'localhost',function(){
