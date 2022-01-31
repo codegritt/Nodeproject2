a40f9778eaa2ba5e8dc8dbdccfa27f7f8b65cd45
Gokul <gokulchandan18@gmail.com>
2022-01-31 20:29:04 +0530

    commit

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 
 var http =require('http');
 var events=require('events');
+var express=require('express');
+
+var app=express();
+
+app.get('/',function(req,res){
+    res.send('<h1>Express works</h1>');
+});
+
+app.listen(3000,function(){
+    console.log('server is listening to port 3000');
+});
 
 
 var eventEmitter=new events.EventEmitter();
